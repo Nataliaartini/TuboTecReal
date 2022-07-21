c55721f76f539ceec82f02189348fd1c3f4f2e36
Natalia <nataliaartini@hotmail.com>
2022-07-21 12:33:45 -0300

    urls e settings

--- TuboTec/settings.py
+++ TuboTec/settings.py
@@ -141,3 +141,7 @@ MESSAGE_TAGS = {
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SETTINGS_IPS = [
+    '127.0.0.1',
+]
