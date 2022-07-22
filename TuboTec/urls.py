35fc24eb1b6d457044b256eaa0d2aa721dcb2a7c
Natalia <nataliaartini@hotmail.com>
2022-07-22 07:39:09 -0300

    falta arrumar urls

--- TuboTec/urls.py
+++ TuboTec/urls.py
@@ -19,11 +19,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('produto.urls')),
-    path('perfil/', include('perfil.urls')),
-    path('pedido/', include('pedido.urls')),
+    # path('', include('produto.urls')),
+    # path('perfil/', include('perfil.urls')),
+    # path('pedido/', include('pedido.urls')),
     path('admin/', admin.site.urls),
 
-    path('__debug__/', include('debug_toolbar.urls')),
+    # path('__debug__/', include('debug_toolbar.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
