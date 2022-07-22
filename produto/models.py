a070ed0bb51a731fdac96203d5495324fc8dc076
Natalia <nataliaartini@hotmail.com>
2022-07-22 09:46:47 -0300

    criando model produto

--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    class Meta:
+        db_table = 'produto'
+        verbose_name = 'Produto'
+        verbose_name_plural = 'Produtos'
+        ordering = ['nome']
+        unique_together = ['nome', 'marca']
+        index_together = ['nome', 'marca']
+        managed = True
+        permissions = (
+            ('view_produto', 'Can view produto'),
+            ('add_produto', 'Can add produto'),
+            ('change_produto', 'Can change produto'),
+            ('delete_produto', 'Can delete produto'),
+        )
+
+    pass
